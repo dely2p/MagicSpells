f5d1a4993677d6c87a436734fe14f980618f481d
dely2p <lhyal2p@gmail.com>
2019-02-22 02:59:57 +0900

    Add CRUD functions by each of button to test networking

--- MagicSpells/MagicSpells/ViewController.swift
+++ MagicSpells/MagicSpells/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userNameLabel: UITextField!
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    var arrayList = Array<String>()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func createDataButton(_ sender: Any) {
+        Alamofire.request("http://harry-server-zdnnh.run.goorm.io/addUser/\(self.userNameLabel.text!)", method: .post, parameters: ["name" : self.nameLabel.text!, "password" : self.passwordLabel.text!]).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        userNameLabel.text = ""
+        nameLabel.text = ""
+        passwordLabel.text = ""
+    }
+    
+    @IBAction func readDataButton(_ sender: Any) {
+        Alamofire.request("http://harry-server-zdnnh.run.goorm.io/list", method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    @IBAction func updateDataButton(_ sender: Any) {
+        Alamofire.request("http://harry-server-zdnnh.run.goorm.io/updateUser/\(self.userNameLabel.text!)", method: .put, parameters: ["name" : self.nameLabel.text!, "password" : self.passwordLabel.text!]).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        userNameLabel.text = ""
+        nameLabel.text = ""
+        passwordLabel.text = ""
+    }
+    
+    @IBAction func deleteDataButton(_ sender: Any) {
+        Alamofire.request("http://harry-server-zdnnh.run.goorm.io/deleteUser/\(self.userNameLabel.text!)", method: .delete).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+        userNameLabel.text = ""
+        nameLabel.text = ""
+        passwordLabel.text = ""
+    }
+    
+    @IBAction func endEditing(_ sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
 }
 

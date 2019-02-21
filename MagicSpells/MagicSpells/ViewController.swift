15af23f24da4a1251fb4c36ecfd740981141854d
dely2p <lhyal2p@gmail.com>
2019-02-22 00:39:30 +0900

    Create MagicSpells

---
+++ MagicSpells/MagicSpells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicSpells
+//
+//  Created by dely on 2019. 2. 22..
+//  Copyright © 2019년 dely. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

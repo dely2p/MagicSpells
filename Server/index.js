5f137a1a964246fba77f7c03693556d230d7e878
dely2p <lhyal2p@gmail.com>
2019-03-09 01:52:46 +0900

    Fix crawling code to get accurate data by web

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
 var app = express();
 var bodyParser = require("body-parser");
 var session = require("express-session");
@@ -37,13 +38,22 @@ request(options, function(err,res,html){
 	var $ = cheerio.load(html);
 	var index = 5;
 	var magicSpells = {};
-
-	for(var i=0;i<11;i++){
-		if($("#mw-content-text > div > h2:nth-child("+index+")").text()!=""){
+	var flag = false;
+	while(!flag){
+		if($("#mw-content-text > div > h2:nth-child("+index+")").text()!="" || index==148){
 			index++;
 		}
 		var title = $("#mw-content-text > div > ul:nth-child("+index+") > li > b").text();
-		var entitle = $("#mw-content-text > div > ul:nth-child("+index+") > li > span").text().replace('(', '').replace(')', '');
+		if(title==""){
+			flag = false;
+			break;
+		}
+		var entitle;
+		if(index==161 || index>=181){
+			entitle = $("#mw-content-text > div > ul:nth-child("+index+") > li").text().replace(/([가-힣\s]+)\(/g, '').replace(')', '');
+		}else {
+			entitle = $("#mw-content-text > div > ul:nth-child("+index+") > li > span").text().replace('(', '').replace(')', '');
+		}
 		index++;
 
 		var effect = $("#mw-content-text > div > dl:nth-child("+index+") > dd:nth-child(1)").text();
@@ -51,20 +61,18 @@ request(options, function(err,res,html){
 		index++;
 
 		var spell = {"title": title, "effect": effect, "source": source};
-		magicSpells[entitle] = spell;
+			magicSpells[entitle] = spell;
 		// console.log(magicSpells[entitle]);
 	}
-	console.log(magicSpells);
-	module.exports = function(app, fs){
-		fs.writeFile(
-			__dirname + "/../data/spells.json",
-			JSON.stringify(magicSpells, null, "\t"),
-			"utf8",
-			function(err, data) {
-			  result = { success: 1 };
-			  console.log(result);
-			  console.log(err);
-		});
-	};
+	// console.log(magicSpells);
+	fs.writeFile(
+		__dirname + "/data/spells.json",
+		JSON.stringify(magicSpells, null, "\t"),
+		"utf8",
+		function(err, data) {
+		  result = { success: 1 };
+		  console.log(result);
+	});
+
 });
 var router = require("./routes/main")(app, fs);
